b6f9e5eaa8682f8419ddeac0f2dad8342f97bbd6
Dharmaveer Imadabattini <2300031733cseh2g@gmail.com>
2026-09-22 10:23:37 +0530

    Update frontend to use live backend URLs

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const AUTH_URL = 'http://localhost:5000/api';
-const CORE_URL = 'http://localhost:5001/api';
-const PAYMENT_URL = 'http://localhost:5002/api';
+const AUTH_URL = 'https://auth-service-v2-6jie.onrender.com/api';
+const CORE_URL = 'https://core-service-duvc.onrender.com/api';
+const PAYMENT_URL = 'https://payment-service-kl28.onrender.com/api';
 
 export const signup = (data) => axios.post(`${AUTH_URL}/auth/signup`, data);
 export const login = (data) => axios.post(`${AUTH_URL}/auth/login`, data);
